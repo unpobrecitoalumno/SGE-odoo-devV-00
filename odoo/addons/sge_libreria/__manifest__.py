17156fbd82f85a8909432bfd77848f093ff9e549
unpobrecitoalumno <unpobrecitoalumno@canaveral.ies>
2025-02-05 18:12:09 +0100

    Update manifest to describe example module

--- odoo/addons/sge_libreria/__manifest__.py
+++ odoo/addons/sge_libreria/__manifest__.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sge_libreria",
+    'name': "Librería",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Módulo de ejemplo de Sistemas de Gestión Empresarial",
 
     'description': """
-Long description of module's purpose
+Descripción larga<br/>
+Se puede leer yendo a Inicio > Aplicaciones<br/>
+Quitando el filtro Aplicaciones y buscando sge<br/>
+Entrando a Más información sobre sge_libreria este nombre técnico
+viene dado por el nombre del directorio cuando lo creamos con odoo scaffold
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "IES El Cañaveral",
+    'website': "https://aulavirtual33.educa.madrid.org/ies.elcanaveral.mostoles/course/view.php?id=347",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Sales',
+    'version': '17.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
