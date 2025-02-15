b10732601f875966d9682f4a2ad84090a8391099
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2025-02-15 13:59:31 +0100

    Add one2many relationship example between categoría (1) and libro (N)

--- odoo/addons/sge_libreria/models/Categoria.py
+++ odoo/addons/sge_libreria/models/Categoria.py
@@ -20,6 +20,15 @@ class Categoria(models.Model):
     name = fields.Char('Nombre', help="Introduzca nombre de categoría", required=True)
     description = fields.Char('Descripción')
     
+    # Sólo una vez definida una relación many2one con este modelo, podemos hacer la recíproca
+    # Usar atajo oofone2many
+    # Por convención, en relaciones xxx2many, donde en el lado derecho puede haber varios objetos,
+    # ponemos el sufijo _ids
+    # primero indicamos el valor _name del otro modelo que colabora en la relación
+    # segundo indicamos el nombre del atributo many2one (el que genera la clave ajena en aquella tabla)
+    # por último la etiqueta que verá el usuario
+    libro_ids = fields.One2many('sge_libreria.libro', 'categoria_id', string='Libros')
+
     # Además Odoo creará otros atributos internos por defecto
     # id servirá de clave primaria
     # create_uid indicará el usuario que creo un elemento de esta clase
